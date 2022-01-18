f35f9162c972a878ae1303acbc278844d19a1a61
Demoted <53052014+Bbro122@users.noreply.github.com>
2022-01-18 11:47:25 -0600

    Update counting.js

--- counting.js
+++ counting.js
@@ -2,6 +2,7 @@ function newCount(client,msg) {
     let channel = msg.channel
     let cnumber = parseInt(channel.topic)
     if (cnumber+1 == parseInt(msg.content)) {
+        console.log(cnumber+1)
         msg.channel.setTopic(toString(cnumber+1))
     } else {
         msg.delete()
