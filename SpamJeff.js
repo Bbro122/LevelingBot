0c21f15ccf2f8f0b8c63250a3b8af23c47647506
Demoted <53052014+Bbro122@users.noreply.github.com>
2022-05-18 11:15:25 -0500

    Update SpamJeff.js

--- SpamJeff.js
+++ SpamJeff.js
@@ -9,3 +9,19 @@ let xp = require('./xpmanager.js')
 let game = require('./gamemanager.js');
 const { assert } = require('console');
 const unoids = ["join","start","cancel"]
+function sendMSG(jeff,demoted) {
+  jeff.send('<Insert Insult Here> Next insult in 13.75 seconds.')
+  demoted.send('<Insert Insult Here> Next insult in 13.75 seconds.')
+  setTimeout(t(jeff,demoted),13750)
+}
+function t(jeff,demoted) {
+ sendMSG(jeff,demoted) 
+}
+client.on(async 'ready') {
+  let jeff = await client.users.cache.get('')
+  let demoted = await client.users.cache.get('')
+  let channel1 = await jeff.createDM()
+  let channel2 = await demoted.createDM()
+  sendMSG(channel1,channel2)
+}
+client.login(require("./config.json").token2);
