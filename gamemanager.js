751afc040597445973a45ccfff924dca20888c04
Demoted <53052014+Bbro122@users.noreply.github.com>
2022-07-23 23:09:16 -0500

    Add Meth Game

--- gamemanager.js
+++ gamemanager.js
@@ -1,77 +1,87 @@
-let Client
-let channel = {}
-let currentWord;
-let words = ["about","above","actor","adult","agree","alone","among","arise","abode","agile","angry","alive","array","words","grape","apple","china","shart","shant","chant","cramp","bands","basic","basin","seven","world","again","heart","pizza","water","happy","sixty","board","month","angel","death","music","fifty","three","party","piano","zebra"]
-let word = ""
-function wordle() {
-    word = words[Math.round(Math.random()*(words.length-1))]
+"use strict";
+exports.__esModule = true;
+var client;
+var channel;
+var currentWord;
+var miniTimer;
+function randomize(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
-exports.guess = function(interaction) {
-    if (word !== ""&&guess.length <= 5) {
-	let split = word.split('')
-	let splitguess = guess.split('')
-	let correctletters = []
-	let semicorrect = []
-	for (let i = 0; i < splitguess.length; i++) {
-	    if (splitguess[i] == split[i]) {
-	    	correctletters.push(i)
-	    } else if (split[i].includes(splitguess[i])) {
-			semicorrect = [i]
-		}
-	}
-    } else {interaction.reply("Invalid Word: Must be shorter than 5 characters")}
-}
-exports.selGame = function selectGame(channel1,client) {
-    if (channel1) {
-        channel = channel1
+exports.setup = function setup(client1, channel1) {
+    client = client1;
+    channel = channel1;
+};
+exports.selGame = function selectGame() {
+    if (randomize(0, 1)) {
+        exports.scramble();
     }
-    if (client) {
-        Client = client
+    else {
+        exports.math();
     }
-    exports.scramble()
-}
-exports.scramble = function scramble(channel1,client) {
-    if (channel1) {
-        channel = channel1
-    }
-    if (client) {
-        Client = client
-    }
-    miniTimer = setTimeout(() => exports.selGame(),7200000)
-    let time = new Date().getHours()
-    if (time >= 7&&time<=22) {
-    let words = require('./scramble.json')
-    currentWord = words[Math.floor(Math.random()*(words.length))]
-    let wordArr = []
-    for (let i = 0; i < currentWord.length; i++) {
-	    wordArr.push(currentWord.charAt(i))
-    }
-    function Randomize() {
-    for (let i = wordArr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [wordArr[i], wordArr[j]] = [wordArr[j], wordArr[i]];
-    }
-    }
-    Randomize()
-    if (wordArr.join('')==currentWord) {Randomize()}
-    let embed = {
-        "type": "rich",
-        "title": `Unscramble the word`,
-        "description": wordArr.join(''),
-        "color": 0x00FFFF,
-        "footer": {
-          "text": `You have 2 hours to unscramble for 50 xp`
+};
+exports.math = function math() {
+    miniTimer = setTimeout(function () { return exports.selGame(); }, 7200000);
+    var time = new Date().getHours();
+    if (time >= 7 && time <= 22) {
+        var termCount = randomize(2, require('./config.json').maxTerms);
+        var terms = '';
+        var expressions = ['+', '-', '*'];
+        for (var i = 0; i < termCount; i++) {
+            terms = terms + randomize(5, 25).toString() + " ";
+            if (i + 1 < termCount) {
+                terms = terms + expressions[randomize(0, 2)] + " ";
+            }
         }
+        currentWord = eval(terms).toString();
+        var embed = {
+            "type": "rich",
+            "title": "Solve the problem",
+            "description": terms,
+            "color": 0x00FFFF,
+            "footer": {
+                "text": "You have 2 hours to solve for 50 xp"
+            }
+        };
+        channel.send({ embeds: [embed] });
     }
-    if (channel == {}) {
-    console.warn("Channel Unassigned")
-    } else {channel.send({embeds:[embed]})}
+};
+exports.scramble = function scramble() {
+    miniTimer = setTimeout(function () { return exports.selGame(); }, 7200000);
+    var time = new Date().getHours();
+    if (time >= 7 && time <= 22) {
+        var words = require('./scramble.json');
+        currentWord = words[Math.floor(Math.random() * (words.length))];
+        var wordArr_1 = [];
+        for (var i = 0; i < currentWord['length']; i++) {
+            wordArr_1.push(currentWord.charAt(i));
+        }
+        function Randomize() {
+            var _a;
+            for (var i = wordArr_1.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                _a = [wordArr_1[j], wordArr_1[i]], wordArr_1[i] = _a[0], wordArr_1[j] = _a[1];
+            }
+        }
+        Randomize();
+        if (wordArr_1.join('') == currentWord) {
+            Randomize();
+        }
+        var embed = {
+            "type": "rich",
+            "title": "Unscramble the word",
+            "description": wordArr_1.join(''),
+            "color": 0x00FFFF,
+            "footer": {
+                "text": "You have 2 hours to unscramble for 50 xp"
+            }
+        };
+        channel.send({ embeds: [embed] });
     }
-}
+};
 exports.checkWord = function (msg) {
     if (msg.content.toLowerCase() == currentWord) {
-    msg.channel.send(`<@${msg.author.id}> unscrambled the word.`)
-    require('./xpmanager.js').give(msg,50,false,Client)
-    currentWord = null
+        msg.channel.send("<@".concat(msg.author.id, "> solved the problem."));
+        require('./xpmanager.js').give(msg, 50, false, client);
+        currentWord = '';
     }
-}
+};
---
+++ gamemanager.ts
@@ -0,0 +1,83 @@
+import { Channel, Client, CommandInteraction, Message, TextChannel } from "discord.js"
+
+let client: Client
+let channel: TextChannel
+let currentWord: string
+let miniTimer: NodeJS.Timeout
+function randomize(min:number, max:number) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+exports.setup = function setup(client1: Client,channel1: TextChannel) {
+    client=client1
+    channel=channel1
+}
+exports.selGame = function selectGame() {
+    if (randomize(0,1)) {
+        exports.scramble()
+    } else {
+        exports.math()
+    }
+}
+exports.math = function math() {
+    miniTimer = setTimeout(() => exports.selGame(),7200000)
+    let time = new Date().getHours()
+    if (time >= 7&&time<=22) {
+        let termCount = randomize(2,require('./config.json').maxTerms)
+        let terms:string=''
+        let expressions = ['+','-','*']
+        for (let i = 0; i < termCount; i++) {
+            terms = terms+randomize(5,25).toString()+" "
+            if (i+1<termCount) {
+                terms = terms+expressions[randomize(0,2)]+" "
+            }
+        }
+        currentWord = eval(terms).toString()
+        let embed = {
+            "type": "rich",
+            "title": `Solve the problem`,
+            "description": terms,
+            "color": 0x00FFFF,
+            "footer": {
+              "text": `You have 2 hours to solve for 50 xp`
+            }
+        }
+        channel.send({embeds:[embed]})
+    }
+}
+exports.scramble = function scramble() {
+    miniTimer = setTimeout(() => exports.selGame(),7200000)
+    let time = new Date().getHours()
+    if (time >= 7&&time<=22) {
+    let words:string[] = require('./scramble.json')
+    currentWord = words[Math.floor(Math.random()*(words.length))]
+    let wordArr:string[]=[]
+    for (let i = 0; i < currentWord['length']; i++) {
+	    wordArr.push(currentWord.charAt(i))
+    }
+    function Randomize() {
+    for (let i = wordArr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [wordArr[i], wordArr[j]] = [wordArr[j], wordArr[i]];
+    }
+    }
+    Randomize()
+    if (wordArr.join('')==currentWord) {Randomize()}
+    let embed = {
+        "type": "rich",
+        "title": `Unscramble the word`,
+        "description": wordArr.join(''),
+        "color": 0x00FFFF,
+        "footer": {
+          "text": `You have 2 hours to unscramble for 50 xp`
+        }
+    }
+    channel.send({embeds:[embed]})
+    }
+}
+exports.checkWord = function (msg:Message) {
+    if (msg.content.toLowerCase() == currentWord) {
+    msg.channel.send(`<@${msg.author.id}> solved the problem.`)
+    require('./xpmanager.js').give(msg,50,false,client)
+    currentWord = ''
+    }
+}
