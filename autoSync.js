2c654eb8bce74cc0c2f8e27bfb37cecfe01434d4
Demoted <53052014+Bbro122@users.noreply.github.com>
2022-08-26 11:34:30 -0500

    Added Sync System

---
+++ autoSync.js
@@ -0,0 +1,11 @@
+const {spawn} = require('child_process')
+function sync() {
+    spawn('git',['pull'])
+    child.on('close', (code) => {
+        console.log(`Exitted with code: ${code}`)
+        setTimeout({reSync()},300000)
+    });
+}
+function reSync() {
+    sync()
+}
