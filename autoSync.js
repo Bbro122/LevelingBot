adc5ddfb1fb9f49c28c88221f5c9b08f60289f5c
Demoted <53052014+Bbro122@users.noreply.github.com>
2022-08-27 13:58:19 -0500

    Fix AutoSync

--- autoSync.js
+++ autoSync.js
@@ -2,7 +2,10 @@ const { spawn } =  require('child_process')
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-while (true) {
-    await sleep(150000)
-    spawn('git',['pull'])
+async function sync() {
+    while (true) {
+        await sleep(150000)
+        spawn('git',['pull'])
+    }
 }
+sync()
