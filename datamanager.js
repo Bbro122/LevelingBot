d1a39cc1b446bf7ae486d4c7a47e27a2730f2a06
Demoted <53052014+Bbro122@users.noreply.github.com>
2022-10-29 13:29:50 -0500

    tt

--- datamanager.js
+++ datamanager.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const { Guild } = require('discord.js');
 let fs = require('fs');
 let data = {};
 data.write = function (data) {
--- datamanager.ts
+++ datamanager.ts
@@ -1,3 +1,5 @@
+import { Guild } from "discord.js"
+
 let fs = require('fs')
 export type UserProfile = {
     "id": string,
@@ -5,38 +7,55 @@ export type UserProfile = {
     "level": number,
     "gems": number
     "items": {
-        type:string,
-        display:{name:string,value:string,inline:false}
-        data:any
+        type: string,
+        display: { name: string, value: string, inline: false }
+        data: any
     }[],
-    "epoch":number
+    "epoch": number
 }
 export type UserData = UserProfile[]
-export type Server = {id: string, users: UserData, gameChan?: string | undefined, countChan?: string | undefined,unoForum?: string | undefined}
-export type ServerData = {servers: Server[]}
-let data:any = {}
-data.write = function(data:ServerData) {
-    fs.writeFileSync('./serverdata.json',data)
+export type Server = { id: string, userProfiles: UserData, gameConfig: { gameChannel: undefined | string, disabledGames: undefined | string[], delay: number }, unoForum?: string | undefined }
+export type ServerData = { servers: Server[] }
+let data: any = {}
+data.server = function (this: any, server: Guild | string) {
+    if (server instanceof Guild) {
+        this.id = server.id
+        this.userProfiles = []
+        this.userCount = server.memberCount
+        this.countChannel = undefined
+        this.gameConfig = { gameChannel: undefined, disabledGames: undefined, delay: 7200000 }
+        this.unoForum = undefined
+    } else {
+        this.id = server
+        this.userProfiles = []
+        this.userCount = 0
+        this.countChannel = undefined
+        this.gameConfig = { gameChannel: undefined, disabledGames: undefined, delay: 7200000 }
+        this.unoForum = undefined
+    }
 }
-data.read = function() {
+data.write = function (data: ServerData) {
+    fs.writeFileSync('./serverdata.json', data)
+}
+data.read = function () {
     return require('./serverdata.json')
 }
-data.get = function(id:string) {
-    let sdata:ServerData = data.read()
+data.get = function (id: string) {
+    let sdata: ServerData = data.read()
     let server = sdata.servers.find(server => server.id == id)
     if (server) {
         return server
     } else {
-        sdata.servers.push({id: id, users: []})
-        return {id: id, users: []}
+        sdata.servers.push(new data.server(id))
+        return { id: id, users: [] }
     }
 }
-data.getuser = function(server:Server,uid:string) {
-    let user = server.users.find(user => user.id == uid)
+data.getuser = function (server: Server, uid: string) {
+    let user = server.userProfiles.find(user => user.id == uid)
     if (user) {
         return user
     } else {
-        let newuser = { id: uid, xp: 0, level: 0, gems: 0, items: [],epoch:0}
+        let newuser = { id: uid, xp: 0, level: 0, gems: 0, items: [], epoch: 0 }
         return newuser
     }
 }
