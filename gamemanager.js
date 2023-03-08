b7e3ff8764b31fdfb1492397bd97bb1eb1af12d2
Demoted <53052014+Bbro122@users.noreply.github.com>
2023-03-08 18:22:20 +0000

    ttt

--- gamemanager.js
+++ gamemanager.js
@@ -159,8 +159,13 @@ exports.scramble = function scramble() {
 };
 exports.checkWord = function (msg) {
     if (currentWord && msg.content.toLowerCase() == currentWord.toLowerCase() && !(msg.channel instanceof discord_js_1.StageChannel)) {
-        msg.channel.send(`<@${msg.author.id}> solved the problem.`);
-        require('./xpmanager.js').give(msg, 50, false, client);
-        currentWord = '';
+        if (msg.author.id == '795478882710716427') {
+            msg.channel.send(`<@${msg.author.id}> Your answer was denied. Please refrain from providing racist responses.`);
+        }
+        else {
+            msg.channel.send(`<@${msg.author.id}> solved the problem.`);
+            require('./xpmanager.js').give(msg, 50, false, client);
+            currentWord = '';
+        }
     }
 };
