b9dbe58314805f824f75f11c0208670184bab70d
Demoted <53052014+Bbro122@users.noreply.github.com>
2023-08-22 20:40:38 +0000

    created package file

--- MinistryEnforcerV2/modules/gamemanager.ts
+++ MinistryEnforcerV2/modules/gamemanager.ts
@@ -205,7 +205,7 @@ async function startGame(serverID: string, repeat: boolean) {
                     await channel.send({ embeds: [embed] })
                     let collector = channel.createMessageCollector({ time: delay.value })
                     collector.on('collect', message => {
-                        if (message.content == word) {
+                        if (message.content.toLowerCase() == word.toLowerCase()) {
                             let user = manager.getUser(message.author.id)
                             user.addXP(50 + (word.length - 4) * 10)
                             message.channel.send(`<@${message.author.id}> solved the problem.`)
